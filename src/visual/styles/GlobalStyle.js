dce921dbf5d5281d321a9e67b26b9b599d3f1faf
Gosch <dev@gosch.io>
2020-06-22 01:35:22 -0300

    Implements Poppins font

--- src/visual/styles/GlobalStyle.js
+++ src/visual/styles/GlobalStyle.js
@@ -1,15 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { COLORS } from 'visual/constants';
+import { Poppins } from 'visual/fonts';
+
 export const GlobalStyle = createGlobalStyle`
+  ${Poppins}
+
   body, html {
     min-height: 100vh;
+    background-color: ${COLORS.dark};
   }
 
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Roboto', serif;
+    font-family: 'Poppins', sans-serif;
     font-weight: normal;
   }
 
