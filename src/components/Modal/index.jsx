7c883ae6f8c8193347dacaaceaf57baeefdfee78
Gosch <dev@gosch.io>
2020-06-23 05:56:40 -0300

    Improve tabindex when modal is closed

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -6,14 +6,23 @@ import { TEXTS } from 'logic/texts';
 import { Container, Content, Close, CloseOverlay } from './styles';
 
 export function Modal({ children, close, opened }) {
+  const tabIndex = opened ? 0 : -1;
+
   return (
-    <Container opened={opened} aria-hidden={!opened}>
+    <Container opened={opened} aria-hidden={!opened} role="dialog">
       {/* Button overlay for close the modal */}
-      <CloseOverlay onClick={close}>{TEXTS.modal.close}</CloseOverlay>
+      <CloseOverlay onClick={close} tabIndex={tabIndex}>
+        {TEXTS.modal.close}
+      </CloseOverlay>
 
       <Content>
-        <Close title={TEXTS.modal.close} onClick={close} />
-        {children}
+        <Close
+          id="period-modal"
+          title={TEXTS.modal.close}
+          onClick={close}
+          tabIndex={tabIndex}
+        />
+        {opened && children}
       </Content>
     </Container>
   );
